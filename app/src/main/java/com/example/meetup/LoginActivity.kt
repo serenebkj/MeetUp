d1908f891ccd619b467e5fcd225b700f140858f1
Serene Babu <59754107+serenebkj2code@users.noreply.github.com>
2020-08-27 21:29:24 +0530

    Small design changes

--- app/src/main/java/com/example/meetup/LoginActivity.kt
+++ app/src/main/java/com/example/meetup/LoginActivity.kt
@@ -2,10 +2,13 @@ package com.example.meetup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
     }
 }
